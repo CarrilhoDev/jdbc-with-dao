2acd00c86a0f5a582124cfcaa4c69a1d6b4990c5
CarrilhoDev <carlos.carrilho@hotmail.com>
2020-05-17 00:08:19 -0400

    Implementing update and deleteById methods

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,10 +15,17 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println("************* Testing findById method *************\n");
+		
 		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
+		
 		System.out.println("\n\n\n");
 
+		
+		
+		
+		
 		System.out.println("************* Testing findByDepartment method *************\n");
 
 		Department dep = new Department(2, null);
@@ -28,6 +35,9 @@ public class Program {
 			System.out.println(obj);
 		}
 		System.out.println("\n\n\n");
+		
+		
+		
 		
 		System.out.println("************* Testing findAll method *************\n");
 
@@ -39,6 +49,9 @@ public class Program {
 		
 		System.out.println("\n\n\n");
 		
+		
+		
+		
 		System.out.println("************* Testing insert method *************\n");
 
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
@@ -46,7 +59,30 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		System.out.println("\n\n\n");
+		
+		
+		
+		
+		System.out.println("************* Testing update method *************\n");
 
+		seller = sellerDao.findById(1);
+		
+		seller.setName("Martha Waine");
+				
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
+		
+		System.out.println("\n\n\n");
+		
+		
+		
+		
+		System.out.println("************* Testing delete method *************\n");
+
+		sellerDao.deletebyId(13);
+		
+		System.out.println("Seller deleted!");
 	}
 
 }
